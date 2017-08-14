a8652eb52e58d64a37f56837e1acd34e4a9ec769
Mohan Agrawal <mohan.agr97@gmail.com>
2017-08-14 13:13:30 +0530

    makes lagrange modular

--- lagrange_interpol.f95
+++ lagrange_interpol.f95
@@ -1,37 +1,43 @@
-program lagrange
+program lagrangei
 
-dimension x(50), y(50)
+write(*,'(A, 1X)', ADVANCE = 'NO') "Please enter x value:"
+read (*, *) xx
+val = Lagrange('lagrange_interpol.in', xx)
+write(*,'(A, f5.2, A, f8.4)') "The value at", xx, " is", val
 
-fn = 0.0
+end program lagrangei
 
-write(*,'(A, 1X)', ADVANCE = 'NO') "Please enter no. of data points and x value:"
-read (*, *) n, xx
-!write(*, *) n, xx
-open(unit=8, file='lagrange_interpol.in', status='unknown')
-open(unit=9, file='lagrange_interpol.out', status='unknown')
-read(8, '(2f11.5)') (x(i), y(i),  i=1,n)
-close(8)
-! write(9, '(2f11.5)') (x(i), y(i),  i=1,n)
-! close(9)
-!write(*, *) "closed 6"
-do i = 1, n
-	!write(*,*) "p is", pval
-	fn = fn + P(x, i, n, xx)*y(i)
-end do
-
-write(*,'(A, f5.2, A, f8.4)') "The value at", xx, " is", fn
-
-end program lagrange
-
-real function P(x, i, n, xx)
-	dimension x(n)
-	prod = 1.0
-	do j = 1, n
-		if(j==i) then
-			cycle
+function Lagrange(datafile, xx)
+	real :: x(50), y(50), fn = 0.0
+	character(*) :: datafile
+	integer :: stat
+	index = 1
+	open(unit=8, file='lagrange_interpol.in', status='unknown')
+	do
+		!write(*,*) 'reading...'
+		read(8, '(2f11.5)', IOSTAT=stat) x(index), y(index)
+		!write(*,*) x(index), y(index)
+		if (stat < 0) then
+			exit
+		else if (stat > 0) then
+			write(*,*) "Something went wrong while reading the file!"
+			exit
+		else
+			index = index + 1
 		end if
-		prod = prod * ((xx - x(j)) / (x(i) - x(j)))
 	end do
-	f = prod
+	close(8)
+	n = index - 1
+	do i = 1, n
+		prod = 1.0
+		do j = 1, n	
+			if(j==i) then
+				cycle
+			end if
+			prod = prod * ((xx - x(j)) / (x(i) - x(j)))
+		end do
+		fn = fn + prod * y(i)
+	end do
+	Lagrange = fn
 	return
-end function P
+end function Lagrange
