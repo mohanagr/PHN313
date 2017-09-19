9248ce6c87366a0dd98b834a1279beafb5f8270f
Mohan Agrawal <mohan.agr97@gmail.com>
2017-09-19 20:21:31 +0530

    Bug fix in Euler's ODE method

---
+++ ODE Euler method/euler.f95
@@ -0,0 +1,31 @@
+subroutine euler_solver(x, y, df, x0, y0, xf, n)
+! Solve ODE using Euler's method.
+! Assumes a function named 'derivative_func' that provides first derivative values
+! ---------
+! Arguments
+! ---------
+! x  : array of x values
+! y  : array of y values
+! df : array of first derivative values at x, y! x0 : initial x value
+! y0 : initial y value
+! xf : final x value
+! n  : number of steps
+	real, dimension(0:50) :: x, y, df
+	h = (xf - x0)/real(n)
+	eps = 1.0E-09
+	x(0) = x0
+	y(0) = y0
+	df(0) = derivative_func(x0, y0)
+	do  i = 1, n
+		x(i) = x0 + i*h
+		y(i) = y(0) + df(i-1) * i * h
+		do
+			df(i) = func(x(i), y(i))
+			y_new = y(i-1) + (df(i) + df(i-1)) * h * 0.5
+			if( abs(y(i) - y_new) <= eps ) then
+				exit
+			end if
+			y(i) = y_new
+		end do
+	end do
+end subroutine euler_solver
--- ODE Euler method/euler_main.f95
+++ ODE Euler method/euler_main.f95
@@ -1,42 +1,20 @@
-! args: function, x array, y array, initial x, initial y, interval end, number of steps, 
-subroutine euler_solver(x, y, x0, y0, xf, n)
-	real, dimension(0:50) :: x, y, df
-	h = (xf - x0)/real(n)
-	eps = 1.0E-09
-	x(0) = x0
-	y(0) = y0
-	df(0) = func(x0, y0)
-	do  i = 1, n
-		x(i) = x0 + (i-1)*h
-		y(i) = y(i-1) + df(i-1) * h
-		y_prev = 100000
-		do
-			if( abs(y(i) - y_prev) <= eps ) then
-				exit
-			end if
-			y_prev = y(i)
-			df(i) = func(x(i), y(i))
-			y(i) = y(0) + (df(i) + df(i-1)) * h * 0.5
-		end do
-	end do
-end subroutine euler_solver
-
-function func(x, y)
-	f = x + y
+function derivative_func(x, y)
+	derivative_func = x + y
 	return
 end function
 
 program euler_main
 
+external :: euler_solver
 real, dimension(0:50) :: x, y, df
 write(*,'(A, 1X)', ADVANCE = 'NO') "Please enter initial x, y, final x and steps:"
 read(*, *) x0, y0, xf, n
 
-call euler_solver(x, y, x0, y0, xf, n)
+call euler_solver(x, y, df, x0, y0, xf, n)
 
 open(unit=8, file="euler.out", status="unknown")
 
-write(8, '(3F9.4)') (x(i), y(i), df(i), i = 0, n+1)
+write(8, '(3F9.4)') (x(i), y(i), df(i), i = 0, n)
 
 close(8)
 
