1c678c13fb802841bdd15f64b45beb792f92e99a
Mohan Agrawal <mohan.agr97@gmail.com>
2017-11-01 19:34:52 +0530

    Debug least square

--- Ordinary Least Square regression/ols1.f95
+++ Ordinary Least Square regression/ols1.f95
@@ -1,28 +1,32 @@
 program ols
-	external linenumber
-	real:: x(50), y(50), beta(0:50), rmse
-	integer :: stat
+	real:: x(50), y(50), beta(0:50), rmse, cc
+
+	write(*,'(A, 1X)', ADVANCE = 'NO') "Please enter number of data points:"
+	read (*, *) n
 	write(*,'(A, 1X)', ADVANCE = 'NO') "Please enter x value where to predict:"
 	read (*, *) xx
-
-	n = linenumber('ols1.in')
-	write(*,*) n, "lines"
-	open(unit=8, file='ols1.in', status='unknown')
-	read(8, '(2f11.5)', IOSTAT=stat) x(index), y(index)
+	open(unit=8, file='ols2.in', status='unknown')
+	read(8, '(2f11.5)') (x(index), y(index), index = 1, n)
 	close(8)
 
-	do i = 1, n
-		y(i) = log(y(i))
-		!write(*,*) y(i)
-	end do
+	! Do the required preprocessing of data here if needed
+	! E.g. converting y to log(y) etc.
 
-	call fit(x, y, beta, n, rmse)
+	! do i = 1, n
+	! 	y(i) = log(y(i))
+	! 	!write(*,*) y(i)
+	! end do
+
+	! Linear fit only
+	call fit(x, y, beta, n, rmse, cc)
 
 	y_predict = predict(beta, xx)
 
-	y_predict = exp(y_predict) !TRANSFORMING AGAIN
+	! y_predict = exp(y_predict) !TRANSFORMING AGAIN
 
-	write(*,'(A, f11.5, A, f11.5, A, f11.5)') "The value at", xx, " is", y_predict, " with RMSE", rmse
+	write(*,'(A, f10.5, A, f10.5, A, f10.5, A, f10.5)') "The value at", xx, " is", y_predict, " with RMSE", rmse, " and cc", cc
+
+	write(*, '(A, 2F10.4)') "The coefficients are :", beta(0), beta(1)
 
 end program ols
 
@@ -33,14 +37,14 @@ function predict(beta, x)
 	return
 end function
 
-subroutine fit(x, y, beta, n, rmse)
+subroutine fit(x, y, beta, n, rmse, cc)
 	real :: x(50), y(50), beta(0:50), xysum = 0.0, xsum = 0.0, ysum =0.0, xsquaresum = 0.0
 	!n = size(y)
 	do i = 1, n
 		ysum = ysum + y(i)
 		xsum = xsum + x(i)
-		xsquaresum = x(i)**2
-		xysum = x(i)*y(i)
+		xsquaresum = xsquaresum + x(i)**2
+		xysum = xysum + x(i)*y(i)
 	end do
 	xmean = xsum/float(n)
 	ymean = ysum/float(n)
@@ -48,10 +52,14 @@ subroutine fit(x, y, beta, n, rmse)
 	beta(0) = ymean - beta(1)*xmean
 	!write(*,*) ymean, xmean
 	rmse = 0.0
+	Sy = 0.0
+	S = 0.0
 	do i = 1, n
 		y_p = predict(beta, x(i))
-		rmse = rmse + (y(i) - y_p)**2
+		Sy = Sy + (y(i) - ymean)**2
+		S = rmse + (y(i) - y_p)**2
 	end do
-	rmse = (rmse/n)**0.5
+	rmse = (S/float(n))**0.5
+	cc = ((Sy - S)/Sy)**0.5
 end subroutine
 
